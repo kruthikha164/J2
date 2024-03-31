d34a8778286cd1c0edb1a0536bfcd5d81c9b9225
01fe22bca164 <156194273+kruthikha164@users.noreply.github.com>
2024-03-31 21:34:40 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,29 +1,35 @@
-import java.util.Scanner;
-class calculator{
-    void add(int a,int b){
-        System.out.println("Addition "+(a+b));
+class Calculator {
+    void add(int a, int b) {
+        System.out.println("Addition " + (a + b));
     }
-    void substract(int a,int b){
-        System.out.println("Substraction "+(a-b));
+
+    void subtract(int a, int b) {
+        System.out.println("Subtraction " + (a - b));
     }
-    void multi(int a,int b){
-        System.out.println("product "+(a*b));
+
+    void multiply(int a, int b) {
+        System.out.println("Product " + (a * b));
     }
-    void division(int a,int b){
-        System.out.println("division "+(a/b));
+
+    void division(int a, int b) {
+        if (b != 0) {
+            System.out.println("Division " + (a / b));
+        } else {
+            System.out.println("Cannot divide by zero");
+        }
     }
 }
-class Main{
 
-    public static void main(String args[]){
-        System.out.println("Enter two numbers");
-        Scanner s=new Scanner(System.in);
-        int a=s.nextInt();
-        int b=s.nextInt();
-        calculator c=new calculator();
-        c.add(a,b);
-        c.substract(a,b);
-        c.multi(a,b);
-        c.division(a,b);
+public class Main {
+
+    public static void main(String args[]) {
+        int a = 10; // Change the value of 'a' to a desired number
+        int b = 5; // Change the value of 'b' to a desired number
+
+        Calculator c = new Calculator();
+        c.add(a, b);
+        c.subtract(a, b);
+        c.multiply(a, b);
+        c.division(a, b);
     }
 }
